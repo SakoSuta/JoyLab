45715431301a28d8806837bca0c4621f977d9c54
Emilie <emiliemontpre@outlook.com>
2022-06-17 19:20:33 +0200

    page panier refaite

--- note.php
+++ note.php
@@ -19,3 +19,115 @@ public function last(){
         ORDER BY produits.date DESC LIMIT 3");
 }
 ?>
+
+<table>
+    <thead>
+        <tr>
+            <th class="CHAKRASemiBold font24">Produit</th>
+            <th class="CHAKRASemiBold font24">Manette</th>
+            <th class="CHAKRASemiBold font24">Prix</th>
+            <th class="CHAKRASemiBold font24">Supprimer</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <td>
+                <div class="ContenuProduit">
+                    <div class="Info">
+                        <img src="..\public\img\Article\CouleurRougeB.png" alt="">
+                        <div class="InfoProduit">
+                            <div class="Article">
+                                <p class="CHAKRASemiBold font24">Couleur Rouge</p>
+                            </div>
+                            <p class="policeCHAKRA font24">Catégories :</p>
+                            <p class="policeCHAKRA font24">Couleur Unique</p>
+                        </div>
+                    </div>
+                </div>
+            </td>
+            <td class="policeCHAKRA font24"><div class="ContenuProduit">PS4</div></td>
+            <td class="policeCHAKRA font24"><div class="ContenuProduit">64,99€</div></td>
+            <td><div class="ContenuProduit"><a href=""><img src="../public/img/Icon/Black/Trash.svg" alt=""></a></div></td>
+        </tr>
+        <tr>
+            <td>
+                <div class="ContenuProduit">
+                    <div class="Info">
+                        <img src="..\public\img\Article\CouleurRougeB.png" alt="">
+                        <div class="InfoProduit">
+                            <div class="Article">
+                                <p class="CHAKRASemiBold font24">Couleur Rouge</p>
+                            </div>
+                            <p class="policeCHAKRA font24">Catégories :</p>
+                            <p class="policeCHAKRA font24">Couleur Unique</p>
+                        </div>
+                    </div>
+                </div>
+            </td>
+            <td class="policeCHAKRA font24">
+                <div class="ContenuProduit">
+                    <p>PS4</p>
+                </div>
+            </td>
+            <td class="policeCHAKRA font24">
+                <div class="ContenuProduit">
+                    <p>64,99€</p>
+                </div>
+            </td>
+            <td>
+                <div class="ContenuProduit">
+                    <a href=""><img src="../public/img/Icon/Black/Trash.svg" alt=""></a>
+                </div>
+            </td>
+        </tr>
+    </tbody>
+</table>
+
+Table {
+        width: 818px;
+        background-color: $blanc;
+        border: 4px solid $vert;
+        border-radius: 10px;
+
+        thead {
+          border: 4px solid $vert;
+          tr {
+            th {
+              border: 4px solid $vert;
+              padding: 25px 0px;
+            }
+          }
+        }
+
+        tbody {
+          tr {
+            border: 4px solid $vert;
+            td {
+              border: 4px solid $vert;
+              .ContenuProduit {
+                padding: 20px 0px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+
+                .Info {
+                  img {
+                    width: 200px;
+                    margin-bottom: 10px;
+                  }
+
+                  .InfoProduit {
+                    display: flex;
+                    flex-direction: column;
+                    align-items: center;
+
+                    .Article {
+                      padding-bottom: 15px;
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
